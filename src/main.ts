c5d0228ae542c646c98027f139b4ecdd575d357a
James Glenn <jglenn1@gmail.com>
2017-10-22 20:35:56 -0500

    simple cleanups, moved buildParams to a separate util file

--- src/main.ts
+++ src/main.ts
@@ -1,54 +1,38 @@
 import * as request from "request-promise"
+import { buildParams } from "./util"
 
 export interface ITags {
   [key: string]: string;
 }
 
 export class cGG {
+  private baseUrl = "http://api.champion.gg/v2/"
   key: string
   constructor(userInput: string) {
     this.key = userInput
   }
   champions(tags: ITags = {}) {
-    let params = ""
-    Object.keys(tags).forEach(key => {
-      params += "&" + key + "=" + tags[key]
-    })
-    return request(`http://api.champion.gg/v2/champions?api_key=${this.key}${params}`)
+    const params = buildParams(tags);
+    return request(`${this.baseUrl}champions?api_key=${this.key}${params}`)
   }
   matchupsByRole(id: number, role: string, tags: ITags = {}) {
-    let params = ""
-    Object.keys(tags).forEach(key => {
-      params += "&" + key + "=" + tags[key]
-    })
-    return request(`http://api.champion.gg/v2/champions/${id}/${role}/matchups?elo=SILVER&api_key=${this.key}${params}`)
+    const params = buildParams(tags);
+    return request(`${this.baseUrl}champions/${id}/${role}/matchups?elo=SILVER&api_key=${this.key}${params}`)
   }
   matchupsByChamp(id: number, tags: ITags = {}) {
-    let params = ""
-    Object.keys(tags).forEach(key => {
-      params += "&" + key + "=" + tags[key]
-    })
-    return request(`http://api.champion.gg/v2/champions/${id}/matchups?api_key=${this.key}${params}`)
+    const params = buildParams(tags);
+    return request(`${this.baseUrl}champions/${id}/matchups?api_key=${this.key}${params}`)
   }
   averagesByChampion(id: number, tags: ITags = {}) {
-    let params = ""
-    Object.keys(tags).forEach(key => {
-      params += "&" + key + "=" + tags[key]
-    })
-    return request(`http://api.champion.gg/v2/champions/${id}?api_key=${this.key}${params}`)
+    const params = buildParams(tags);
+    return request(`${this.baseUrl}champions/${id}?api_key=${this.key}${params}`)
   }
   generalSiteInformation(tags: ITags = {}) {
-    let params = ""
-    Object.keys(tags).forEach(key => {
-      params += "&" + key + "=" + tags[key]
-    })
-    return request(`http://api.champion.gg/v2/general?api_key=${this.key}${params}`)
+    const params = buildParams(tags);
+    return request(`${this.baseUrl}general?api_key=${this.key}${params}`)
   }
   overall(tags: ITags = {}) {
-    let params = ""
-    Object.keys(tags).forEach(key => {
-      params += "&" + key + "=" + tags[key]
-    })
-    return request(`http://api.champion.gg/v2/overall?api_key=${this.key}${params}`)
+    const params = buildParams(tags);
+    return request(`${this.baseUrl}overall?api_key=${this.key}${params}`)
   }
 }
