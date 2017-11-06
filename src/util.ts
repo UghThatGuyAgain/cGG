804eab77230ef816ccc69314d0308186220cfba9
UghThatGuyAgain <jarrettklink1@gmail.com>
2017-11-05 23:06:51 -0500

    Updated package.json to finalize it. inb4 massive amounts of bugs

--- src/util.ts
+++ src/util.ts
@@ -1,4 +1,5 @@
 import { ITags } from './main'
+import http = require("http")
 
 export function buildParams(tags: ITags): string {
     let params = ""
@@ -7,4 +8,17 @@ export function buildParams(tags: ITags): string {
     });
 
     return params;
+}
+export function makeRequest(url) {
+  return new Promise((resolve, reject) => {
+    const request = http.get(url, (response) => {
+      if (response.statusCode < 200 || response.statusCode > 299) {
+        reject(new Error('Failed to load page, status code: ' + response.statusCode));
+      }
+      const body = []
+      response.on('data', (chunk) => body.push(chunk));
+      response.on('end', () => resolve(body.join('')));
+    });
+    request.on('error', (err) => reject(err))
+  })
 }
